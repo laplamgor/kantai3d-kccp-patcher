289b833d8f21f83df22887389f4f51cfa2cd957c
laplamgor <laplamgor@gmail.com>
2020-10-24 23:38:55 +0800

    Refactor GLSL

--- Kantai3d/kcs2/js/main.js/patcher.js
+++ Kantai3d/kcs2/js/main.js/patcher.js
@@ -1,6 +1,12 @@
 const { readFileSync } = require("fs")
 const { join } = require("path")
 
+
+let vert = readFileSync(join(__dirname, './kantai3d.vert'));
+let frag = readFileSync(join(__dirname, './kantai3d.frag'));
+
+
+
 module.exports = (file, contents) => {
 
     contents = contents.toString();
@@ -84,134 +90,7 @@ window.displacementFilter.uniforms.offset = [flip * mousex *1.3
     return contents + `;
 
 'use strict';
-PIXI.DepthPerspectiveFilter = new PIXI.Filter(
-\`
-#ifdef GL_ES
-precision highp float;
-#endif
-
-attribute vec2 aVertexPosition;
-attribute vec2 aTextureCoord;
-varying vec2 vTextureCoord;
-
-
-uniform mat3 projectionMatrix;
-
-
-void main(void){
-
-    vTextureCoord = aTextureCoord;
-    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-}
-\`
-
-    , 
-\`
-precision mediump float;
-uniform vec2 offset;
-
-uniform sampler2D uSampler;
-uniform sampler2D displacementMap;
-
-uniform float textureScale;
-uniform float textureWidth;
-uniform float textureHeight;
-uniform float frameWidth;
-uniform float frameHeight;
-
-uniform float padding;
-
-uniform vec4 filterArea;
-uniform vec4 filterClamp;
-
-
-varying vec2 vTextureCoord;
-varying vec4 vColor;
-uniform vec4 dimensions;
-uniform vec2 mapDimensions;
-uniform float scale;
-uniform float focus;
-
-
-vec2 mapCoordDepth(vec2 coord) {
-  return vec2((coord[0] * frameWidth - padding) / textureWidth / textureScale,
-              (coord[1] * frameHeight - padding) / textureHeight / textureScale);
-}
-
-vec2 mapCoord2(vec2 coord) {
-  return vec2(coord[0] * frameWidth / textureWidth / textureScale,
-              coord[1] * frameHeight / textureHeight / textureScale);
-}
-
-const float compression = 1.0;
-const float dmin = 0.0;
-const float dmax = 1.0;
-
-#define MAXSTEPS 600.0
-float steps = max(MAXSTEPS * length(offset), 30.0);
-
-void main(void) {
-
-  float aspect = dimensions.x / dimensions.y;
-  vec2 scale2 =
-      vec2(scale * min(1.0, 1.0 / aspect), scale* min(1.0, aspect)) * vec2(1, -1);
-  mat2 baseVector =
-      mat2(vec2((0.5 - focus) * (offset) - (offset) / 2.0) * scale2,
-           vec2((0.5 - focus) * (offset) + (offset) / 2.0) * scale2);
-
-
-  vec2 pos = (vTextureCoord);
-  mat2 vector = baseVector;
-
-  float dstep = compression / (steps - 1.0);
-  vec2 vstep = (vector[1] - vector[0]) / vec2((steps - 1.0));
-
-  vec2 posSumLast = vec2(0.0);
-  vec2 posSum = vec2(0.0);
-
-  float weigth = 1.0;
-  float dpos;
-  float dposLast;
-
-
-  for (float i = 0.0; i < MAXSTEPS; ++i) {
-    vec2 vpos = pos + vector[1] - i * vstep;
-    dpos = 1.0 - i * dstep;
-
-    float depth = texture2D(displacementMap, mapCoordDepth(vpos)).r;
-
-
-    if (texture2D(uSampler,vpos)[3] == 0.0) {
-        depth = 0.0;
-    }
-
-
-    depth = clamp(depth, dmin, dmax);
-    float confidence;
-
-#define CORRECT 100
-#if CORRECT == 10
-#define CORRECTION_MATH +((vec2((depth - dpos) / (dstep)) * vstep))
-#else
-#define CORRECTION_MATH
-#endif
-
-    if (dpos > depth) {
-      posSumLast = (vpos CORRECTION_MATH);
-      dposLast = dpos;
-    } else {
-      posSum = (vpos CORRECTION_MATH);
-      weigth = (depth - dposLast) / (dstep);
-      break;
-    }
-  };
-
-  gl_FragColor = texture2D(
-      uSampler,
-      (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum);
-
-}
-\`  );
+PIXI.DepthPerspectiveFilter = new PIXI.Filter(\``+ vert + `\`, \`` + frag + `\`);
 
 PIXI.DepthPerspectiveFilter.apply = function(filterManager, input, output)
 {
